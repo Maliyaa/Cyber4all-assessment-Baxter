fca2360a778b5d5acda39e73ee82eb1bca99c41b
Maliya <maliyabaxter@gmail.com>
2022-03-18 19:22:29 -0400

    I updated this file with the method used for deleteAQuest

--- backend/updatingAQuest.js
+++ backend/updatingAQuest.js
@@ -1,43 +1,20 @@
-//code needed for routing 
-import { response } from 'express';
-import {
-    ReasonPhrases, 
-    StatusCodes,
-    getReasonPhrase, 
-    getStatusCode, 
-} from 'http-status-codes'; 
 
-response
-        .status(getStatusCode('No Content'))
-        .send({
-            error: 'Quest was updated in the database'
-        });
-response
-        .status(getStatusCode('Bad Request'))
-        .send({
-            error: 'Route herold does not match the Quest\'s herold in database'
-        });
-response
-        .status(getStatusCode('Internal Server Error'))
-        .send({
-            error: 'Hero was not found for given ID'
-        });
-// code from https://medium.com/@9cv9official/what-are-get-post-put-patch-delete-a-walkthrough-with-javascripts-fetch-api-17be31755d28
-
-import { response } from "express";
-
-fetch('http://localhost:3000/heroes/:heroId/quests/:questId', {
-  method: 'PATCH', 
-  body: JSON.stringify({
-      completed: true
-  }),
-  headers: {
-      "Content-type": "application/json; charset=UTF-8"
-  }  
-})
-.then(response => response.json())
-.then(json => console.log(json))
-{
-   "description": "Scome description" //Vscode is throwing a syntax error because I am using a ":" it wants ";" but that's not what the body calls for
-
+//code to use XHTML request from https://www.geeksforgeeks.org/delete-request-using-xmlhttprequest-by-making-custom-http-library/
+function easyHTTP(){
+    this.http = new XMLHttpRequest();
 }
+easyHTTP.prototype.delete = function (url, callback){
+    this.http.open("PATCH", url, true);
+
+    let self = this;
+    this.http.onload = function(){
+        if(self.http.status === 204) {
+            callback(null, "Quest was updated in the database");
+        } else if(self.http.status === 400) {
+            callback(null, "Route herold does not match the Quest\'s herold in the database");
+        } else{
+            callback("Hero of Quest was not found for given IDs" + self.http.status);
+        }
+    };
+    this.http.send();
+}; 
