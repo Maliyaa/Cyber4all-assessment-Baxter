6f1a6a86904c96f1dcddff4e70aabd90f1d1e911
Maliya <maliyabaxter@gmail.com>
2022-03-18 18:16:25 -0400

    adding routing to backend task 2

--- backend/createAQuest.js
+++ backend/createAQuest.js
@@ -1,3 +1,25 @@
+//code needed for routing 
+import { response } from 'express';
+import {
+    ReasonPhrases, 
+    StatusCodes,
+    getReasonPhrase, 
+    getStatusCode, 
+} from 'http-status-codes'; 
+
+response
+        .status(StatusCodes.CREATED)
+        .send(ReasonPhrases.CREATED);
+response
+        .status(StatusCodes.NOT_FOUND)
+        .send({
+            error: getReasonPhrase(StatusCodes.NOT_FOUND)
+        });
+response
+        .status(getStatusCode('Internal Server Error'))
+        .send({
+            error: 'Hero was not found for given ID'
+        });
 //creating a post request for Creating a Quest using XMLHttpRequest
 //code found on: https://reqbin.com/code/javascript/wzp2hxwh/javascript-post-request-example#:~:text=To%20send%20an%20HTTP%20POST,subscribe%20to%20the%20onreadystatechange%20event.
 let url = "http://localhost:3000/heroes/:id/quests";
@@ -19,5 +41,5 @@ let quest = {
     "description": "Save the cat from the burning tree!"
 }
 
-//code needed for routing 
+
 
